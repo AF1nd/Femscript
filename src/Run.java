80503e9d8ffa40b612ab15ef438bb091188a9d7c
AF1nd <leonidsopov90@mail.ru>
2024-09-07 22:38:56 +0700

    Update Run.java

--- src/Run.java
+++ src/Run.java
@@ -2,7 +2,7 @@ import Parser.AST.BlockNode;
 import Parser.AST.Node;
 
 public interface Run {
-    public Object run(Node node);
+    public void run(Node node);
     public Object run(Node node, BlockNode parent_statement);
     public Object run(Node node, BlockNode parent_statement, Boolean assertion);
 }
